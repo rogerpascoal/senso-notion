80afac7360257447ef9f0c836d99958c64f20021
rogerpascoal <66312258+rogerpascoal@users.noreply.github.com>
2023-04-24 10:26:02 -0300

    Update and rename senso-lab.js to senso-lab.html

--- senso-lab.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-JBZFXC8RJX"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-JBZFXC8RJX');
-</script>
