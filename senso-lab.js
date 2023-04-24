27ab488a310f74e7491665d1cd04a87cce07210d
rogerpascoal <66312258+rogerpascoal@users.noreply.github.com>
2023-04-24 09:58:31 -0300

    Create senso-lab.js

---
+++ senso-lab.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JBZFXC8RJX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JBZFXC8RJX');
+</script>
